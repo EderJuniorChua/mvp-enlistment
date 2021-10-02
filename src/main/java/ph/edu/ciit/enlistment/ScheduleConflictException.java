7026ff78954da5974274618e30fd2fdb93666e75
Eder Junior Chua <eder.chua@ciit.edu.ph>
2021-10-02 16:04:41 +0800

    Fixed directory file system and created ScheduleConflictException

---
+++ src/main/java/ph/edu/ciit/enlistment/ScheduleConflictException.java
@@ -0,0 +1,8 @@
+package ph.edu.ciit.enlistment;
+
+public class ScheduleConflictException extends RuntimeException{
+
+    ScheduleConflictException(String message){
+        super(message);
+    }
+}
