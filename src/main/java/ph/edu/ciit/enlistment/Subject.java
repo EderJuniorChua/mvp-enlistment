9334c797f19f54fb96050fb3341934df347fc6a7
Eder Junior Chua <eder.chua@ciit.edu.ph>
2021-10-11 16:47:43 +0800

    8. A subject may or may not have one or more prerequisite subjects.

--- src/main/java/ph/edu/ciit/enlistment/Subject.java
+++ src/main/java/ph/edu/ciit/enlistment/Subject.java
@@ -2,16 +2,25 @@ package ph.edu.ciit.enlistment;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
+
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.apache.commons.lang3.Validate.isTrue;
+import static org.apache.commons.lang3.Validate.notNull;
 
 public class Subject {
     private final String subjectID;
+    private final Collection<Subject> prerequisites;
 
-    Subject(String subjectID){
+    Subject(String subjectID, Collection<Subject> prerequisites){
         isBlank(subjectID);
         isTrue(StringUtils.isAlphanumeric(subjectID),
                 "subjectID must be alphanumeric, was: " + subjectID);
         this.subjectID = subjectID;
+        notNull(prerequisites);
+        this.prerequisites = new HashSet<>(prerequisites);
+        this.prerequisites.removeIf(Objects::isNull);
     }
 }
