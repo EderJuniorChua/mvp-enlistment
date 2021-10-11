d80f630425c6e5959085d4848100f4fb51bc7efd
Eder Junior Chua <eder.chua@ciit.edu.ph>
2021-10-11 15:36:17 +0800

    Revert "Refactored Room to final immutable version by adding 'null' as default Room"
    Room must be modifiable, I just realized Ooops. Revert the Commit/Push.
    
    This reverts commit c7838524

--- src/main/java/ph/edu/ciit/enlistment/Section.java
+++ src/main/java/ph/edu/ciit/enlistment/Section.java
@@ -3,21 +3,24 @@ package ph.edu.ciit.enlistment;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.util.Collections;
-
 import static org.apache.commons.lang3.Validate.*;
 import static org.apache.commons.lang3.StringUtils.*;
 
 class Section {
     private final String sectionId;
     private final Schedule schedule;
-    private final Room room;
+    private Room room;
     private Subject subject;
     //TODO: (Allen) 2. A section must have an instructor. A class Instructor is pre requisite.
     //TODO: (Allen) 3. An instructor cannot teach two or more sections with overlapping schedule. This is to be made in new instructor class.
 
     Section (String sectionId, Schedule schedule) {
-        this(sectionId, schedule, null);
+        isBlank(sectionId);
+        isTrue(StringUtils.isAlphanumeric(sectionId),
+                "sectionId must be alphanumeric, was: " + sectionId);
+        notNull(schedule);
+        this.sectionId = sectionId;
+        this.schedule = schedule;
     }
 
     Section (String sectionId, Schedule schedule, Room room) {
