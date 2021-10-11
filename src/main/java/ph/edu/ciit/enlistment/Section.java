4ecd64905bab93dcc524b071e1d75d191a5a6e09
Eder Junior Chua <eder.chua@ciit.edu.ph>
2021-10-11 15:39:17 +0800

    refactored Section() constructor to method overloading version for better readability.

--- src/main/java/ph/edu/ciit/enlistment/Section.java
+++ src/main/java/ph/edu/ciit/enlistment/Section.java
@@ -15,12 +15,7 @@ class Section {
     //TODO: (Allen) 3. An instructor cannot teach two or more sections with overlapping schedule. This is to be made in new instructor class.
 
     Section (String sectionId, Schedule schedule) {
-        isBlank(sectionId);
-        isTrue(StringUtils.isAlphanumeric(sectionId),
-                "sectionId must be alphanumeric, was: " + sectionId);
-        notNull(schedule);
-        this.sectionId = sectionId;
-        this.schedule = schedule;
+        this(sectionId, schedule, null);
     }
 
     Section (String sectionId, Schedule schedule, Room room) {
@@ -28,6 +23,7 @@ class Section {
         isTrue(StringUtils.isAlphanumeric(sectionId),
                 "sectionId must be alphanumeric, was: " + sectionId);
         this.sectionId = sectionId;
+        notNull(schedule);
         this.schedule = schedule;
         this.room = room;
     }
