03b5bf24a76dd64348573c4d4fc0977cdd739e5c
Eder Junior Chua <eder.chua@ciit.edu.ph>
2021-10-11 15:12:57 +0800

    Added TODO: for Allen, Bullet points 2 and 3

--- src/main/java/ph/edu/ciit/enlistment/Section.java
+++ src/main/java/ph/edu/ciit/enlistment/Section.java
@@ -10,6 +10,8 @@ class Section {
     private final String sectionId;
     private final Schedule schedule;
     private Room room;
+    //TODO: (Allen) 2. A section must have an instructor. A class Instructor is pre requisite.
+    //TODO: (Allen) 3. An instructor cannot teach two or more sections with overlapping schedule. This is to be made in new instructor class.
 
     Section (String sectionId, Schedule schedule) {
         isBlank(sectionId);
