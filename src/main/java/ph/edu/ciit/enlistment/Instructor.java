2db747af3f9158fbc2bbfd7be99bee2791e1659a
Allen Aouie <allen.ardaniel@ciit.edu.ph>
2021-10-14 13:41:41 +0800

    added schedule conflict on Instructor.java

--- src/main/java/ph/edu/ciit/enlistment/Instructor.java
+++ src/main/java/ph/edu/ciit/enlistment/Instructor.java
@@ -6,22 +6,25 @@ import static org.apache.commons.lang3.Validate.*;
 
 class Instructor {
     private final int instructorID;
+    private final Schedule schedule;
     private final Collection<Section> sections;
-
-    Instructor(int instructorID, Collection<Section> sections) {
+    Instructor(int instructorID, Schedule schedule, Collection<Section> sections) {
         notNull(sections);
+        isTrue(instructorID >= 0,"instructorID must be non-zero, was : " + instructorID);
         this.sections = new HashSet<>(sections);
         this.sections.removeIf(Objects::isNull);
         this.instructorID = instructorID;
+        this.subject = subject;
     }
-
     Instructor(int instructorID) {
         this(instructorID, Collections.emptyList());
     }
-
     void procured_section(Section newSection) {
         notNull(newSection);
-        sections.forEach(currSection -> currSection.hasScheduleConflict(newSection));
-        sections.add(newSection);
+        if (this.sections.isSimilar(other.sections))
+        throw new SameSubjectException(
+                    "This section" + this + "and other section " + other + " have same schedule."
+            );
     }
+
 }
