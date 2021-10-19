ace4e21dc149f8a9f8d00e8240a0495381a21e6d
peejaypel <61832441+peejaypel@users.noreply.github.com>
2021-10-19 23:19:22 +0800

    Added unit test for creating new period with the same start time and end time

--- src/test/java/ph/edu/ciit/enlistment/PeriodTest.java
+++ src/test/java/ph/edu/ciit/enlistment/PeriodTest.java
@@ -9,4 +9,9 @@ public class PeriodTest {
     void create_period_with_end_time_before_start_time(){
         assertThrows(InvalidPeriodException.class, () -> new Period(Hours.H1130, Hours.H0830));
     }
+
+    @Test
+    void create_period_with_start_time_equal_to_end_time(){
+        assertThrows(InvalidPeriodException.class, () -> new Period(Hours.H0830, Hours.H0830));
+    }
 }
